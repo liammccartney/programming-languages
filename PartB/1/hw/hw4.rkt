f2b540a9e3ed718ffa26af4f9ee6f58431da5df6
Liam McCartney <liam.mccartney@gmail.com>
2019-03-22 15:47:15 -0400

    [PartB] HW4 WIP

--- PartB/1/hw/hw4.rkt
+++ PartB/1/hw/hw4.rkt
@@ -3,14 +3,34 @@
 
 (provide (all-defined-out)) ;; so we can put tests in a second file
 
-;; put your code below
-
+; 1 sequence
 (define (sequence low high stride)
   (define aux
-    (lambda(low high stride nums)
-      (if (or (= low high) (> (+ low stride) high))
-          nums
-      (aux (+ low stride) high stride (append nums (cons (+ low stride) null))))))
-  (if (> high low)
-      (aux low high stride (cons low null))
-  null))
+    (lambda (nums low high stride)
+      (cond [(or (> low high) (> (+ low stride) high)) nums]
+            [(null? nums) (aux (append nums (list low (+ low stride))) (+ low stride) high stride)]
+            [(aux (append nums (list (+ low stride))) (+ low stride) high stride)])))
+  (aux null low high stride))
+
+; 2 string-append-map
+(define (string-append-map xs suffix)
+  (map (lambda (x) (string-append x suffix)) xs))
+
+; 3 list-nth-mod
+(define (list-nth-mod xs n)
+      (cond [(null? xs) error "list-nth-mod: empty list"]
+            [(<= n 0) error "list-nth-mod: negative number"]
+            [(car (list-tail xs (remainder n (length xs))))]
+      ))
+
+; 4 stream-for-n-steps
+; It returns a list holding the first n values produced by s in order.
+(define (stream-for-n-steps stream n)
+  (stream-collector stream n null))
+
+(define (stream-collector stream n xs)
+  (let ([next (stream)])
+  (cond [(<= n 0) xs]
+        [(stream-collector (cdr next) (- n 1) (append xs (list (car next))))])))
+
+(define (funny-number-stream) (list "lol"))
